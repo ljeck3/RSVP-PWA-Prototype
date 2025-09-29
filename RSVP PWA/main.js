527ce778e6636046bd91e7c6dfcfc45e61c91af5
lj-1003 <levijustin24@gmail.com>
2025-09-29 13:22:08 -0500

    Updated form

--- RSVP PWA/main.js
+++ RSVP PWA/main.js
@@ -1,9 +1,6 @@
 function rsvpYes() {
-    console.log("Yes function ran")
     const nameInput = document.getElementById("nameInput").value;
     const guestInput = document.getElementById("guestInput").value;
-    console.log(nameInput)
-    console.log(guestInput)
 
     fetch("https://script.google.com/macros/s/AKfycbxOheWBesYVx6k_SKqZT0kCLMw7pDkfLscQtc2BSm8svUZnLkz87viyiBgOIkLFji1H/exec", {
     method: "POST",
@@ -17,12 +14,8 @@ function rsvpYes() {
 }
 
 function rsvpNo() {
-    console.log("Yes function ran")
     const nameInput = document.getElementById("nameInput").value;
     const guestInput = document.getElementById("guestInput").value;
-    console.log(nameInput)
-    console.log(guestInput)
-
 
     fetch("https://script.google.com/macros/s/AKfycbxOheWBesYVx6k_SKqZT0kCLMw7pDkfLscQtc2BSm8svUZnLkz87viyiBgOIkLFji1H/exec", {
     method: "POST",
@@ -34,5 +27,3 @@ function rsvpNo() {
         })
     })
 }
-
-
