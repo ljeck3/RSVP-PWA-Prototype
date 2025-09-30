16c982e843ef57836efac3e86f46309cf070320c
Levi Eck <ljeck3@mail.fhsu.edu>
2025-09-29 23:25:30 -0500

    Re-adding files

---
+++ main.js
@@ -0,0 +1,47 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("SW Registered!");
+        console.log(registration);
+    }).catch(error => {
+        console.log("SW Registration Failed!");
+        console.log(error);
+    });
+}
+
+function rsvpYes() {
+    const nameInput = document.getElementById("nameInput").value;
+    const guestInput = document.getElementById("guestInput").value;
+
+    fetch("https://script.google.com/macros/s/AKfycbxOheWBesYVx6k_SKqZT0kCLMw7pDkfLscQtc2BSm8svUZnLkz87viyiBgOIkLFji1H/exec", {
+    method: "POST",
+    body: JSON.stringify({ 
+        name: nameInput, 
+        guest: guestInput,
+        yes: "X",
+        no: ""
+        })
+    })
+}
+
+function rsvpNo() {
+    const nameInput = document.getElementById("nameInput").value;
+    const guestInput = document.getElementById("guestInput").value;
+
+    fetch("https://script.google.com/macros/s/AKfycbxOheWBesYVx6k_SKqZT0kCLMw7pDkfLscQtc2BSm8svUZnLkz87viyiBgOIkLFji1H/exec", {
+    method: "POST",
+    body: JSON.stringify({ 
+        name: nameInput, 
+        guest: guestInput,
+        yes: "",
+        no: "X"
+        })
+    })
+}
+
+
+//For Materialize CSS nav bar
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
